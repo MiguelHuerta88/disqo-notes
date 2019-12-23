d06afdc36484c22fcd9f689cfa8754107fadb9b3
Miguel Huerta <miguelhuerta@Miguels-MBP.attlocal.net>
2019-12-22 18:31:40 -0800

    got the PATCH route to work. Had to set postman to be form-urlencoded otherwise it doesnt pick up the parameters

--- api/routes/api.php
+++ api/routes/api.php
@@ -28,6 +28,7 @@ Route::middleware('auth:api')->group(function() {
 	Route::get('notes', 'Api\NotesController@all');
 	Route::post('note', 'Api\NotesController@create')->name('note.create');
 	Route::get('/note/{notes}', 'Api\NotesController@view')->middleware('owner');
-	Route::post('note/{notes}/edit', 'Api\NotesController@update')->middleware('owner');
+	//Route::post('note/{notes}/edit', 'Api\NotesController@update')->middleware('owner');
+	Route::patch('note/{notes}', 'Api\NotesController@update')->middleware('owner');
 	Route::delete('/note/{notes}', 'Api\NotesController@delete')->middleware('owner');
 });
